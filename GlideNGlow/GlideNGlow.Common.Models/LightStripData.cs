10d0f2084d37c74f357191a0c0ec752e105abd88
massimo <massimodb@hotmail.com>
2024-01-10 16:35:21 +0100

    Fix AppSettings.cs

--- GlideNGlow/GlideNGlow.Common.Models/LightStripData.cs
+++ GlideNGlow/GlideNGlow.Common.Models/LightStripData.cs
@@ -2,8 +2,8 @@ namespace GlideNGlow.Common.Models;
 
 public struct LightStripData
 {
-    public List<float> DistanceTillNext { get; set; }
-    public List<float> Length { get; set; }
-    public List<int> Leds { get; set; }
+    public float DistanceTillNext { get; set; }
+    public float Length { get; set; }
+    public int Leds { get; set; }
     
 }
--- GlideNGlow/GlideNGlow.Common.Models/Settings/AppSettings.cs
+++ GlideNGlow/GlideNGlow.Common.Models/Settings/AppSettings.cs
@@ -7,6 +7,6 @@ public class AppSettings
     public List<Guid> AvailableGamemodes { get; set; } = new();
     public bool ForceGamemode { get; set; } = false;
     public bool LightingToggle { get; set; } = true;
-    public List<LightButtonData> Strips { get; set; } = new();
-    public List<LightStripData> Buttons { get; set; } = new();
+    public List<LightButtonData> Buttons { get; set; } = new();
+    public List<LightStripData> Strips { get; set; } = new();
 }
