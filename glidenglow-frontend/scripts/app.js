87fc4f25b423b63908dac2932ba0ea01838bd97a
MartensRemco <remco.martens@student.howest.be>
2024-01-11 13:40:27 +0100

    Feat: Current gamemode panel

--- glidenglow-frontend/scripts/app.js
+++ glidenglow-frontend/scripts/app.js
@@ -79,10 +79,73 @@ function checkSettings() {
     });
   });
 }
+function checkAvailableGamemodes() {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', function () {
+      console.log(this.id + ': ' + this.checked);
+      const data = {
+        id: this.id,
+        checked: this.checked,
+      };
+      /*
+      // Assuming your API endpoint is https://example.com/api/updateCheckboxStatus
+      fetch('https://example.com/api/updateCheckboxStatus', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((responseData) => {
+          console.log(responseData); // Log the API response if needed
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });*/
+    });
+  });
+}
+function checkCurrentGamemodes() {
+  // Get all gamemode-box elements
+  const gamemodeBoxes = document.querySelectorAll('.gamemode-box');
+
+  // Add click event listener to each gamemode-box
+  gamemodeBoxes.forEach(function (box) {
+    box.addEventListener('click', function () {
+      // Toggle background color
+      if (this.classList.contains('selected')) {
+        this.classList.remove('selected');
+        console.log(
+          `Current gamemode - ${this.querySelector('.title').innerText}: false`
+        );
+      } else {
+        // Remove 'selected' class from all gamemode-box elements
+        gamemodeBoxes.forEach(function (otherBox) {
+          otherBox.classList.remove('selected');
+        });
+
+        this.classList.add('selected');
+        console.log(
+          `Current gamemode - ${this.querySelector('.title').innerText}: true`
+        );
+      }
+    });
+  });
+}
 
 document.addEventListener('DOMContentLoaded', (event) => {
   checkUrl();
   checkAdmin();
   checkLogout();
   checkSettings();
+  checkAvailableGamemodes();
+  checkCurrentGamemodes();
 });
