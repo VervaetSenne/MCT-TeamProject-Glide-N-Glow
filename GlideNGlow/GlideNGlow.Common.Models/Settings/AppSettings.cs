4ce549d6d3382b11568b37a6a55e536b9b3a3ec5
Magyx <w.magyx@gmail.com>
2024-01-15 21:58:44 +0100

    Fix missing property allow user change

--- GlideNGlow/GlideNGlow.Common.Models/Settings/AppSettings.cs
+++ GlideNGlow/GlideNGlow.Common.Models/Settings/AppSettings.cs
@@ -5,7 +5,8 @@ public class AppSettings
     public string Ip { get; set; }
     public Guid? CurrentGamemode { get; set; }
     public List<Guid> AvailableGamemodes { get; set; } = new();
-    public bool ForceGamemode { get; set; } = false;
+    public Guid? ForceGamemode { get; set; }
+    public bool AllowUserSwitching { get; set; }
     public bool LightingToggle { get; set; } = true;
     public List<LightButtonData> Strips { get; set; } = new();
     public List<LightStripData> Buttons { get; set; } = new();
