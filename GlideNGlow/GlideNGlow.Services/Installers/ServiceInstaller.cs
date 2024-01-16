11602e69ebbc661c5b3ef10ae8cc8b4aaf1ea08a
Magyx <w.magyx@gmail.com>
2024-01-16 21:58:47 +0100

    Add required services to installer

--- GlideNGlow/GlideNGlow.Services/Installers/ServiceInstaller.cs
+++ GlideNGlow/GlideNGlow.Services/Installers/ServiceInstaller.cs
@@ -1,5 +1,6 @@
 ﻿using GlideNGlow.Common.Models.Settings;
 using GlideNGlow.Core.Services.Installers;
+using GlideNGlow.Services.Abstractions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options.Implementations.Extensions;
@@ -12,6 +13,8 @@ public static class ServiceInstaller
     {
         return services
             .InstallCore(config)
+            .AddScoped<ISettingsService, SettingsService>()
+            .AddScoped<IAvailableGameService, AvailableGameService>()
             .ConfigureWritable<AppSettings>(config, nameof(AppSettings));
     }
 }
