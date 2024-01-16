d4e4764ea4ec087a0da8ea882dbb5a7f6a1d4160
Magyx <w.magyx@gmail.com>
2024-01-16 10:37:21 +0100

    Add description field

--- GlideNGlow/GlideNGlow.Core.Models/Game.cs
+++ GlideNGlow/GlideNGlow.Core.Models/Game.cs
@@ -4,6 +4,7 @@ public class Game
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public required string Name { get; set; }
+    public required string Description { get; set; }
     public required byte[] Image { get; set; }
     public required string AssemblyName { get; set; }
 }
