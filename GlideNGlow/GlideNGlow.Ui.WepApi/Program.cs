bb6b95e81aee0c2d6192091506558148c760b14c
Magyx <w.magyx@gmail.com>
2024-01-17 10:43:27 +0100

    Add seeding to in memory database

--- GlideNGlow/GlideNGlow.Ui.WepApi/Program.cs
+++ GlideNGlow/GlideNGlow.Ui.WepApi/Program.cs
@@ -1,4 +1,11 @@
+using GlideNGlow.Common.Models;
+using GlideNGlow.Common.Models.Settings;
+using GlideNGlow.Core.Data;
+using GlideNGlow.Core.Models;
 using GlideNGlow.Services.Installers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options.Implementations;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,6 +34,108 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+#if DEBUG
+    var scope = app.Services.CreateScope();
+
+    var dbContext = scope.ServiceProvider.GetRequiredService<GlideNGlowDbContext>();
+    if (dbContext.Database.IsInMemory())
+    {
+        dbContext.Games.AddRange(new []
+        {
+            new Game
+            {
+                Id = default,
+                Name = "Follow",
+                Description = "Follow the light and beat your meat.",
+                Image = Array.Empty<byte>(),
+                AssemblyName = "",
+                Settings = JsonConvert.SerializeObject(new Setting[]
+                {
+                    new()
+                    {
+                        Type = nameof(TimeSpan),
+                        Name = "Time",
+                        Required = true
+                    }
+                })
+            } // Add more for testing
+        });
+        dbContext.Entries.AddRange(new []
+        {
+            new Entry
+            {
+                Name = "Warre",
+                Score = "8282"
+            },
+            new Entry
+            {
+                Name = "Remco",
+                Score = "5044"
+            },
+            new Entry
+            {
+                Name = "Masimo",
+                Score = "5425"
+            },
+            new Entry
+            {
+                Name = "Senne",
+                Score = "8755"
+            },
+        });
+
+        var appsettings = scope.ServiceProvider.GetRequiredService<IWritableOptions<AppSettings>>();
+        appsettings.Update(s =>
+        {
+            s.Strips = new List<LightstripData>
+            {
+                new()
+                {
+                    Id = 0,
+                    Leds = 300,
+                    Length = 3,
+                    DistanceFromLast = 0
+                },
+                new()
+                {
+                    Id = 0,
+                    Leds = 300,
+                    Length = 3,
+                    DistanceFromLast = 0
+                },
+                new()
+                {
+                    Id = 0,
+                    Leds = 300,
+                    Length = 3,
+                    DistanceFromLast = 0
+                }
+            };
+            s.Buttons = new List<LightButtonData>
+            {
+                new()
+                {
+                    ButtonNumber = 0,
+                    MacAddress = "6F:C0:00:FF:00:13",
+                    DistanceFromStart = 10
+                },
+                new()
+                {
+                    ButtonNumber = 1,
+                    MacAddress = "5B:D9:F9:89:6B:60",
+                    DistanceFromStart = 20
+                },
+                new()
+                {
+                    ButtonNumber = 2,
+                    MacAddress = "86:6A:50:B5:8E:F6",
+                    DistanceFromStart = 30
+                },
+            };
+        });
+    }
+#endif
 }
 
 app.UseHttpsRedirection();
