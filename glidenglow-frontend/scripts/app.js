e7003cac3779649456c888bf51d7a2024e805364
MartensRemco <remco.martens@student.howest.be>
2024-01-18 15:00:14 +0100

    Feat: New admin panel + Gamemodes carous

--- glidenglow-frontend/scripts/app.js
+++ glidenglow-frontend/scripts/app.js
@@ -2,11 +2,14 @@
 const isAdminLoggedIn = localStorage.getItem('AdminLoggedIn') === 'true';
 var fetchdom = 'http://localhost:5165';
 
-//Available gamemodes vars
-var availableGamemodes;
+//Gamemodes var
+let gamemodesTable;
+let AllowGamemodeSwitchLoadState;
+
+//Buttons var
+let buttonTable;
 
 //Current gamemode vars
-var currentGamemodes;
 
 function checkUrl() {
   // Check if the URL pathname is /admin.html
@@ -29,7 +32,7 @@ function checkUrl() {
   }
 }
 function checkAdmin() {
-  // Show/hide the admin navigation item based on the user's login status
+  // Show/hide the admin navigation itemS based on the user's login status
   const navItemAdmin = document.getElementById('nav-item-admin');
   const navItemLogoutAdmin = document.querySelector('.nav-item-logout-admin');
   if (isAdminLoggedIn) {
@@ -50,66 +53,12 @@ function checkLogout() {
     console.log('logout');
   });
 }
-function checkSettings() {
-  const toggleCheckboxes = document.querySelectorAll(
-    '.toggle input[type="checkbox"]'
-  );
-  toggleCheckboxes.forEach(function (checkbox) {
-    checkbox.addEventListener('change', function () {
-      // Get the corresponding option text
-      const optionText =
-        this.closest('.grid-option').querySelector('.option-text').innerText;
-
-      // Log the state (true or false) and the option text
-      console.log(`${optionText} : ${this.checked}`);
-
-      // Prepare the data to send to the API
-      const data = {
-        optionText: optionText,
-        isChecked: this.checked,
-      };
-
-      // Send the data to the API using fetch
-      var fetchlink = ``;
-      if (data.optionText == 'User gamemode switch') {
-        fetchlink = `${fetchdom}/admin/allow-game-switch/${data.isChecked}`;
-        console.log(fetchlink);
-      }
-      if (data.optionText == 'Gamemode force') {
-        fetchlink = `${fetchdom}/admin/force-gamemode/${data.isChecked}`;
-        console.log(fetchlink);
-      }
-      if (data.optionText == 'Lighting') {
-        fetchlink = `${fetchdom}/admin/lightning/${data.isChecked}`;
-        console.log(fetchlink);
-      }
-      if (data.optionText == 'Start calibration') {
-        fetchlink = '';
-      }
-
-      fetch(fetchlink, {
-        method: 'POST', // or 'PUT', 'DELETE', etc., depending on your API
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      })
-        .then((result) => {
-          // Handle the API response if needed
-          console.log('API Response:', result);
-        })
-        .catch((error) => {
-          // Handle errors
-          console.error('Error sending data to API:', error);
-        });
-    });
-  });
-}
-function checkAvailableGamemodes() {
-  const checkboxStates = {}; // Object to store checkbox states
-
-  //fetch data
-  fetch(`${fetchdom}/gamemode/all`)
+function handleGamemodes() {
+  /*
+    GAMEMODES GET ALL GAMEMODES
+  */
+  console.log('gamemodes function');
+  fetch(`${fetchdom}/gamemode/settings`)
     .then((response) => {
       if (!response.ok) {
         throw new Error(
@@ -119,71 +68,211 @@ function checkAvailableGamemodes() {
       return response.json();
     })
     .then((gamemodes) => {
-      // Assuming your API response is an array of gamemodes
       console.log(gamemodes);
-      let html = '';
-      for (const gamemode of gamemodes) {
-        html += `<div class="grid-option">
-                  <span class="option-text-availablegamemodes">${gamemode.name}</span>
-                  <label class="checkbox-container">
-                    <input type="checkbox" id="${gamemode.id}" />
-                    <span class="checkmark"></span>
-                  </label>
-                </div>
-                <hr class="option-divider" />`;
+      let html = '<table class="rounded-table">';
 
-        // Initialize checkbox state in the object -- get from api later
-        checkboxStates[gamemode.id] = false;
+      // Add table headers
+      html += `
+        <tr>
+          <th>Name</th>
+          <th>Set available</th>
+          <th>Force</th>
+        </tr>
+      `;
+
+      for (const gamemode of gamemodes.gamemodes) {
+        console.log(gamemode);
+        html += `
+          <tr>
+            <td>${gamemode.name}</td>
+            <td>
+              <label class="checkbox-container">
+                <input type="checkbox" class="set-available-checkbox" data-id="${gamemode.id}" />
+                <span class="checkmark"></span>
+              </label>
+            </td>
+            <td>
+              <button class="table-button table-button-force-gamemodes" id="${gamemode.id}">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-lock"
+                >
+                  <rect
+                    width="18"
+                    height="11"
+                    x="3"
+                    y="11"
+                    rx="2"
+                    ry="2"
+                  />
+                  <path d="M7 11V7a5 5 0 0 1 10 0v4" />
+                </svg>
+              </button>
+            </td>
+          </tr>`;
       }
-      availableGamemodes.innerHTML = html;
 
-      // Attach event listener to each checkbox
-      const checkboxes = document.querySelectorAll(
-        '.checkbox-container input[type="checkbox"]'
+      html += '</table>';
+      gamemodesTable.innerHTML = html;
+
+      /*
+      SETT GAMEMODE AVAILABLE CHECKBOX
+    */
+
+      // Add event listener to all checkboxes with the class "set-available-checkbox"
+      const setAvailableCheckboxes = document.querySelectorAll(
+        '.set-available-checkbox'
       );
-      var ListCheckedModes = [];
+      setAvailableCheckboxes.forEach((checkbox) => {
+        checkbox.addEventListener('change', () => {
+          console.log(
+            `Set Gamemode Available Checkbox id: ${checkbox.dataset.id} state:`,
+            checkbox.checked
+          );
+          var setGamemodeAvailableId = checkbox.dataset.id;
 
-      checkboxes.forEach((checkbox) => {
-        checkbox.addEventListener('change', function () {
-          checkboxStates[this.id] = this.checked; // Update checkbox state
-          if (this.checked) {
-            ListCheckedModes.push(this.id);
+          if (checkbox.checked) {
+            // If checkbox is checked, POST to set gamemode available state
+            fetch(`${fetchdom}/gamemode/available/${setGamemodeAvailableId}`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            })
+              .then((result) => {
+                // Handle the API response if needed
+                console.log('API Response - set gamemode available:', result);
+              })
+              .catch((error) => {
+                // Handle errors
+                console.error(
+                  'Error sending data to API - set gamemode available:',
+                  error
+                );
+              });
           } else {
-            const index = ListCheckedModes.indexOf(this.id);
-            if (index !== -1) {
-              ListCheckedModes.splice(index, 1);
-            }
+            // If checkbox is unchecked, DELETE to remove gamemode available state
+            fetch(`${fetchdom}/gamemode/available/${setGamemodeAvailableId}`, {
+              method: 'DELETE',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            })
+              .then((result) => {
+                // Handle the API response if needed
+                console.log(
+                  'API Response - remove gamemode available:',
+                  result
+                );
+              })
+              .catch((error) => {
+                // Handle errors
+                console.error(
+                  'Error sending data to API - remove gamemode available:',
+                  error
+                );
+              });
           }
+        });
+      });
 
-          console.log(ListCheckedModes);
+      /*
+      GAMEMODES ALLOW GAMEMODE SWITCH
+      */
 
-          // Post to the API
-          fetch(`${fetchdom}/admin/available`, {
-            method: 'POST',
+      // Add event listener to the toggle
+      const allowGamemodeToggle = document.getElementById(
+        'AllowGamemodeToggle'
+      );
+      allowGamemodeToggle.addEventListener('change', () => {
+        console.log('Allow Gamemode Switch:', allowGamemodeToggle.checked);
+        var allowGamemodeSwitchState = allowGamemodeToggle.checked;
+        // Post the allow gamemode switch state to api
+        fetch(
+          `${fetchdom}/gamemode/allow-switching/${allowGamemodeSwitchState}`,
+          {
+            method: 'PUT',
             headers: {
               'Content-Type': 'application/json',
             },
-            body: JSON.stringify(ListCheckedModes),
+          }
+        )
+          .then((result) => {
+            // Handle the API response if needed
+            console.log('API Response - allow gamemode switch:', result);
+          })
+          .catch((error) => {
+            // Handle errors
+            console.error(
+              'Error sending data to API - allow gamemode switch:',
+              error
+            );
+          });
+      });
+      /*
+    FORCE GAMEMODE
+  */
+
+      // Add event listener to all force buttons
+      const forceButtons = document.querySelectorAll(
+        '.table-button-force-gamemodes'
+      );
+      forceButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+          console.log('Force Button clicked');
+          console.log(`Force Button id: ${button.id} clicked`);
+          var forceGamemodeId = button.id;
+
+          // Get the corresponding checkbox for the current button
+          const correspondingCheckbox = document.querySelector(
+            `.set-available-checkbox[data-id="${forceGamemodeId}"]`
+          );
+
+          // Toggle the background color
+          button.classList.toggle('gamemode-force-active');
+
+          // Patch the new state to the API endpoint
+          fetch(`${fetchdom}/gamemode/force/${forceGamemodeId}`, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+            },
           })
             .then((result) => {
               // Handle the API response if needed
-              console.log('API Response:', result);
+              console.log('API Response - force gamemode:', result);
             })
             .catch((error) => {
               // Handle errors
-              console.error('Error posting checked modes to API:', error);
+              console.error(
+                'Error sending data to API - force gamemode:',
+                error
+              );
             });
+
+          // Ensure that only one force button is active at a time
+          forceButtons.forEach((otherButton) => {
+            if (otherButton !== button) {
+              otherButton.classList.remove('gamemode-force-active');
+            }
+          });
         });
       });
-    })
-    .catch((error) => {
-      console.error('Error fetching gamemodes:', error);
     });
 }
-
-function checkCurrentGamemodes() {
-  //fetch data
-  fetch(`${fetchdom}/gamemode/all`)
+function handleButtons() {
+  /*
+    BUTTONS -  GET ALL BUTTONS
+  */
+  fetch(`${fetchdom}/button`)
     .then((response) => {
       if (!response.ok) {
         throw new Error(
@@ -192,74 +281,85 @@ function checkCurrentGamemodes() {
       }
       return response.json();
     })
-    .then((gamemodes) => {
-      // Assuming your API response is an array of gamemodes
-      console.log(gamemodes);
-      let html = '';
-      for (const gamemode of gamemodes) {
-        html += `<div class="gamemode-box">
-                  <div class="row">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="40"
-                      height="40"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="lucide lucide-gamepad-2"
-                    >
-                      <line x1="6" x2="10" y1="11" y2="11" />
-                      <line x1="8" x2="8" y1="9" y2="13" />
-                      <line x1="15" x2="15.01" y1="12" y2="12" />
-                      <line x1="18" x2="18.01" y1="10" y2="10" />
-                      <path
-                        d="M17.32 5H6.68a4 4 0 0 0-3.978 3.59c-.006.052-.01.101-.017.152C2.604 9.416 2 14.456 2 16a3 3 0 0 0 3 3c1 0 1.5-.5 2-1l1.414-1.414A2 2 0 0 1 9.828 16h4.344a2 2 0 0 1 1.414.586L17 18c.5.5 1 1 2 1a3 3 0 0 0 3-3c0-1.545-.604-6.584-.685-7.258-.007-.05-.011-.1-.017-.151A4 4 0 0 0 17.32 5z"
-                      />
-                    </svg>
-                  </div>
-                  <div class="row title">${gamemode.name}</div>
-                  <div class="row description">
-                    This is the description for ${gamemode.name}.
-                  </div>
-                </div>`;
+    .then((buttons) => {
+      console.log(buttons);
+      let html = '<table class="rounded-table">';
+
+      // Add table headers
+      html += `
+        <tr>
+                    <th id="button-id">Id</th>
+                    <th id="button-distance">Distance(m)</th>
+                    <th id="button-actions">Actions</th>
+                  </tr>
+      `;
+
+      for (const button of buttons) {
+        html += `
+          <tr>
+                    <td id="button-id-data">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="lucide lucide-circle"
+                      >
+                        <circle cx="12" cy="12" r="10" />
+                      </svg>
+                    </td>
+                    <td id="distance-data">${button.distance}</td>
+                    <td>
+                      <button class="table-button">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          class="lucide lucide-pencil"
+                        >
+                          <path
+                            d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"
+                          />
+                          <path d="m15 5 4 4" />
+                        </svg>
+                      </button>
+                      <button class="table-button">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          class="lucide lucide-trash-2"
+                        >
+                          <path d="M3 6h18" />
+                          <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                          <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                          <line x1="10" x2="10" y1="11" y2="17" />
+                          <line x1="14" x2="14" y1="11" y2="17" />
+                        </svg>
+                      </button>
+                    </td>
+                  </tr>`;
       }
-      currentGamemodes.innerHTML = html;
 
-      // Get all gamemode-box elements
-      const gamemodeBoxes = document.querySelectorAll('.gamemode-box');
-
-      // Add click event listener to each gamemode-box
-      gamemodeBoxes.forEach(function (box) {
-        box.addEventListener('click', function () {
-          // Toggle background color
-          if (this.classList.contains('selected')) {
-            this.classList.remove('selected');
-            console.log(
-              `Current gamemode - ${
-                this.querySelector('.title').innerText
-              }: false`
-            );
-          } else {
-            // Remove 'selected' class from all gamemode-box elements
-            gamemodeBoxes.forEach(function (otherBox) {
-              otherBox.classList.remove('selected');
-            });
-
-            this.classList.add('selected');
-            console.log(
-              `Current gamemode - ${
-                this.querySelector('.title').innerText
-              }: true`
-            );
-          }
-        });
-      });
-    })
-    .catch((error) => {
-      console.error('Error fetching gamemodes:', error);
+      html += '</table>';
+      buttonTable.innerHTML = html;
     });
 }
 
@@ -300,18 +400,15 @@ function renderCarousel() {
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
+  //Gamemode vars
+  gamemodesTable = document.querySelector('.js-gamemodes-table');
+  buttonTable = document.querySelector('.js-buttons-table');
+  //Functions
   checkUrl();
   checkAdmin();
   checkLogout();
-  //checkSettings();
-  //checkAvailableGamemodes();
-  //checkCurrentGamemodes();
+  handleGamemodes();
+  handleButtons();
+  //handleButtons();
   renderCarousel();
-
-  //Available gamemodes vars
-  availableGamemodes = document.querySelector(
-    '.js-available-gamemodes-container'
-  );
-  //Current gamemode vars
-  currentGamemodes = document.querySelector('.js-current-gamemode-container');
 });
