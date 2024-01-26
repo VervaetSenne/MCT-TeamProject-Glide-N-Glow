056f953fc6c890a1e16a6f38b78df1dbf24b4191
Magyx <w.magyx@gmail.com>
2024-01-26 16:18:26 +0100

    Implement appsettings initializer

--- GlideNGlow/GlideNGlow.Common.Options.Extensions/AppSettingsInitializer.cs
+++ GlideNGlow/GlideNGlow.Common.Options.Extensions/AppSettingsInitializer.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using GlideNGlow.Common.Models.Settings;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace GlideNGlow.Common.Options.Extensions;
 
@@ -6,6 +9,25 @@ public static class AppSettingsInitializer
 {
     public static void InitializeAppSettings(this IHostEnvironment environment)
     {
-        
+        var filePath = Path.Combine(environment.ContentRootPath, "appsettings.json");
+        if (!File.Exists(filePath))
+        {
+            File.Create(Path.Combine(environment.ContentRootPath, "appsettings.json"));
+            var text = new
+            {
+                ConnectionString = new
+                {
+                    @default = ""
+                },
+                AppSettings = new AppSettings()
+            };
+            File.WriteAllText(filePath, JsonConvert.SerializeObject(text));
+            throw new Exception("Please fill in 'ip' and 'connectionstring' in the appsettings.json file!");
+        }
+
+        var settings = JsonConvert.DeserializeObject<JObject>(File.ReadAllText(filePath))?.Value<AppSettings>(nameof(AppSettings)) ?? throw new Exception();
+        settings.CurrentSettings = "";
+        settings.CurrentGamemode = null;
+        File.WriteAllText(filePath, JsonConvert.SerializeObject(settings));
     }
 }
