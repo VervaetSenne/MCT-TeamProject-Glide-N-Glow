85d305dc630248aa686f229264dfd178b0e997b7
VervaetSenne <vervaetsenne@student.howest.be>
2024-01-29 17:33:48 +0100

    Apache added to install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,22 @@ sudo mysql -e "GRANT ALL PRIVILEGES ON GlideNGlow.* TO 'glidenglow_user'@'localh
 
 echo "mariadb install succesful"
 
+apt install apache2 -y
+touch /etc/apache2/sites-available/000-default.conf > '
+<VirtualHost *:80>
+    ServerAdmin webmaster@localhost
+    DocumentRoot /app/glidenglow-frontend        
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+'
+
+cp ./GlideNGlow/GlideNGlow.Ui.WebApi/glidenglow-frontend /app/glidenglow-frontend
+
+echo "apache install succesful"
+echo "website copied succesful"
+chmod 666 -R /app/glidenglow-frontend
+
 # install dotnet
 wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x ./dotnet-install.sh
@@ -34,7 +50,7 @@ dotnet ef database update --context GlideNGlowDbContext --startup-project ./Glid
 echo "database up to date"
 
 # build project
-dotnet build ./MCT-TeamProject-Glide-N-Glow/GlideNGlow/GlideNGlow.Ui.WepApi/GlideNGlow.Ui.WepApi.csproj --configuration Release --property:ASPNETCORE_URLS='http://10.10.10.13:5165' --output /app/
+dotnet build ./GlideNGlow/GlideNGlow.Ui.WepApi/GlideNGlow.Ui.WepApi.csproj --configuration Release --property:ASPNETCORE_URLS='http://10.10.10.13:5165' --output /app/
 export ASPNETCORE_URLS=$ASPURL
 export ASPNETCORE_ENVIRONMENT="Production"
 chmod 666 /app/appsettings.json
