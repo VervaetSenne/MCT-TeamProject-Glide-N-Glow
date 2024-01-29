854f4950149dfaac378d4bd8252a5a340fbc6f01
MartensRemco <remco.martens@student.howest.be>
2024-01-29 16:49:24 +0100

    Fix: No scores + changes

--- glidenglow-frontend/scripts/running.js
+++ glidenglow-frontend/scripts/running.js
@@ -104,25 +104,36 @@ function handleRecentScores() {
       console.log('recentScores');
       console.log(recentScores);
       let html = ``;
-      html += `<tr>
-        <th>Username</th>
-        <th>Score</th>
-        <th>`;
-      for (const score of recentScores) {
-        html += `<tr id="score-row-id-${score.playerIndex}">
-        <td>${score.playerName}</td>
-        <td>${score.value}</td>
-        <td>
-          <button class="table-button" id="${score.playerIndex}" onclick="claimScore(this)">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-hand">
-              <path d="M18 11V6a2 2 0 0 0-2-2v0a2 2 0 0 0-2 2v0" />
-              <path d="M14 10V4a2 2 0 0 0-2-2v0a2 2 0 0 0-2 2v2" />
-              <path d="M10 10.5V6a2 2 0 0 0-2-2v0a2 2 0 0 0-2 2v8" />
-              <path d="M18 8a2 2 0 1 1 4 0v6a8 8 0 0 1-8 8h-2c-2.8 0-4.5-.86-5.99-2.34l-3.6-3.6a2 2 0 0 1 2.83-2.82L7 15" />
-            </svg>
-          </button>
-        </td>
-      </tr>`;
+
+      if (recentScores.length === 0) {
+        html += `<tr>
+          <th>Username</th>
+          <th>Score</th>
+          <th>Claim</th>`;
+        html += `<tr>
+          <td colspan="3">No recent scores</td>
+        </tr>`;
+      } else {
+        html += `<tr>
+          <th>Username</th>
+          <th>Score</th>
+          <th>`;
+        for (const score of recentScores) {
+          html += `<tr id="score-row-id-${score.playerIndex}">
+          <td>${score.playerName}</td>
+          <td>${score.value}</td>
+          <td>
+            <button class="table-button" id="${score.playerIndex}" onclick="claimScore(this)">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-hand">
+                <path d="M18 11V6a2 2 0 0 0-2-2v0a2 2 0 0 0-2 2v0" />
+                <path d="M14 10V4a2 2 0 0 0-2-2v0a2 2 0 0 0-2 2v2" />
+                <path d="M10 10.5V6a2 2 0 0 0-2-2v0a2 2 0 0 0-2 2v8" />
+                <path d="M18 8a2 2 0 1 1 4 0v6a8 8 0 0 1-8 8h-2c-2.8 0-4.5-.86-5.99-2.34l-3.6-3.6a2 2 0 0 1 2.83-2.82L7 15" />
+              </svg>
+            </button>
+          </td>
+        </tr>`;
+        }
       }
       recentScoresContainer.innerHTML = html;
     });
